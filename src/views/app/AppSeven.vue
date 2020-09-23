20dc920b0d218febfe675a38805b648e4651c970
lizilong <94648929@qq.com>
2020-09-23 18:49:50 +0800

    pdf

--- src/views/app/AppSeven.vue
+++ src/views/app/AppSeven.vue
@@ -3,7 +3,7 @@
  * @author: lizlong<94648929@qq.com>
  * @since: 2020-09-10 19:59:33
  * @LastAuthor: lizlong
- * @lastTime: 2020-09-11 09:52:45
+ * @lastTime: 2020-09-23 10:43:26
 -->
 <template>
 	<div>
@@ -12,57 +12,39 @@
 </template>
 <script>
 import pdfMake from "pdfmake/build/pdfmake";
-import pdfFonts from "pdfmake/build/vfs_fonts";
-// import pdfFonts from "@/assets/vfs/vfs_fonts.js";
+// import pdfFonts from "pdfmake/build/vfs_fonts";
+import pdfFonts from "@/assets/vfs/vfs_fonts.js";
+// import pdfFonts from "@/assets/vfs/fzyt_vfs_fonts.js";
 pdfMake.vfs = pdfFonts.pdfMake.vfs;
 export default {
 	data() {
 		return {
 			docDefinition: {
 				pageSize: "A4",
-				// defaultStyle: {
-				// 	font: "",
-				// },
+				defaultStyle: {
+					font: "方正小标宋简体",
+				},
 				content: [
 					{
-						layout: "lightHorizontalLines", // optional
 						table: {
-							// headers are automatically repeated if the table spans over multiple pages
-							// you can declare how many rows should be treated as headers
-							headerRows: 1,
-							widths: ["*", "auto", 100, "*"],
-
+							widths: ["*", "*", "*", "*"],
 							body: [
-								["First", "Second", "Third", "The last one"],
-								["Value 1", "Value 2", "Value 3", "Value 4"],
+								["word", "word", "word", "word"],
 								[
-									{ text: "Bold value", bold: true },
-									"Val 2",
-									"Val 3",
-									"Val 4",
+									{ text: "方正小标宋简体", colSpan: 2 },
+									"",
+									"",
+									{ text: "word", rowSpan: 3 },
 								],
+								[
+									{ text: "word", colSpan: 2, rowSpan: 2 },
+									"",
+									"word",
+									"",
+								],
+								["", "", "word", ""],
 							],
 						},
-					},
-					// if you don't need styles, you can use a simple string to define a paragraph
-					"烦烦烦+++++++DASDDADDDThis is a standard paragraph, using default style",
-					// using a { text: '...' } object lets you set styling properties
-					{
-						text: "This paragraph will have a bigger font",
-						fontSize: 15,
-					},
-
-					// if you set the value of text to an array instead of a string, you'll be able
-					// to style any part individually
-					{
-						text: [
-							"This paragraph is defined as an array of elements to make it possible to ",
-							{
-								text: "restyle part of it and make it bigger ",
-								fontSize: 15,
-							},
-							"than the rest.",
-						],
 					},
 				],
 			},
@@ -76,6 +58,52 @@ export default {
 					bold: "Roboto-Medium.ttf",
 					italics: "Roboto-Italic.ttf",
 					bolditalics: "Roboto-Italic.ttf",
+				},
+				仿宋_GB2312: {
+					normal: "FANGSONG_GB2312.ttf",
+					bold: "FANGSONG_GB2312.ttf",
+					italics: "FANGSONG_GB2312.ttf",
+					bolditalics: "FANGSONG_GB2312.ttf",
+				},
+				方正小标宋简体: {
+					normal: "FZXBSJW.ttf",
+					bold: "FZXBSJW.ttf",
+					italics: "FZXBSJW.ttf",
+					bolditalics: "FZXBSJW.ttf",
+				},
+				楷体GB2312: {
+					normal: "KAITI_GB2312.ttf",
+					bold: "KAITI_GB2312.ttf",
+					italics: "KAITI_GB2312.ttf",
+					bolditalics: "KAITI_GB2312.ttf",
+				},
+				黑体: {
+					normal: "KAITI_GB2312.ttf",
+					bold: "KAITI_GB2312.ttf",
+					italics: "KAITI_GB2312.ttf",
+					bolditalics: "KAITI_GB2312.ttf",
+				},
+			};
+			pdfMake.tableLayouts = {
+				exampleLayout: {
+					hLineWidth: function (i, node) {
+						if (i === 0 || i === node.table.body.length) {
+							return 0;
+						}
+						return i === node.table.headerRows ? 2 : 1;
+					},
+					vLineWidth: function (i) {
+						return 0;
+					},
+					hLineColor: function (i) {
+						return i === 1 ? "black" : "#aaa";
+					},
+					paddingLeft: function (i) {
+						return i === 0 ? 0 : 8;
+					},
+					paddingRight: function (i, node) {
+						return i === node.table.widths.length - 1 ? 0 : 8;
+					},
 				},
 			};
 			pdfMake.createPdf(this.docDefinition).open();
