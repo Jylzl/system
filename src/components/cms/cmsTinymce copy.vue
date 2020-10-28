a3d5196d3d3b12d3db8522a3e78b74923e477ce9
lizilong <94648929@qq.com>
2020-10-28 18:26:22 +0800

    tinymce升级

--- src/components/cms/cmsTinymce copy.vue
+++ src/components/cms/cmsTinymce copy.vue
@@ -3,7 +3,7 @@
  * @author: lizlong<94648929@qq.com>
  * @since: 2019-07-30 17:49:09
  * @LastAuthor: lizlong
- * @lastTime: 2020-10-28 14:53:38
+ * @lastTime: 2020-09-04 18:19:59
  -->
 <template>
 	<Editor v-model="html" api-key="udm8u7u1w88b8yqqt0czgf3glqzet1mnbt95at9wv8u6bib3" :init="init"></Editor>
@@ -85,13 +85,13 @@ export default {
 		return {
 			html: "",
 			init: {
-				cache_suffix: "?v=5.5.1",
-				base_url: "/tinymce", // 从另一个位置加载主题和插件
+				cache_suffix: "?v=5.4.1",
 				language_url: "/tinymce/langs/zh_CN.min.js", // 语言包的路径
 				language: "zh_CN", // 语言
 				skin_url: "/tinymce/skins/ui/oxide", // skin路径
 				emoticons_database_url:
 					"/tinymce/plugins/emoticons/emojis.min.js",
+				base_url: "/tinymce", // 从另一个位置加载主题和插件
 				content_css:
 					"/tinymce/skins/content/default/ck_htmledit_views.min.css",
 				body_class: "htmledit_views",
@@ -100,17 +100,126 @@ export default {
 					formatpainter: "/tinymce/plugins/formatpainter/plugin.js",
 					a11ychecker: "/tinymce/plugins/a11ychecker/plugin.js",
 				},
-				plugins:
-					"print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker imagetools textpattern noneditable help formatpainter permanentpen pageembed charmap tinycomments mentions quickbars linkchecker emoticons advtable",
-				tinydrive_token_provider: "URL_TO_YOUR_TOKEN_PROVIDER",
-				tinydrive_dropbox_app_key: "YOUR_DROPBOX_APP_KEY",
-				tinydrive_google_drive_key: "YOUR_GOOGLE_DRIVE_KEY",
-				tinydrive_google_drive_client_id: "YOUR_GOOGLE_DRIVE_CLIENT_ID",
-				mobile: {
-					plugins:
-						"print preview powerpaste casechange importcss tinydrive searchreplace autolink autosave save directionality advcode visualblocks visualchars fullscreen image link media mediaembed template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists checklist wordcount tinymcespellchecker a11ychecker textpattern noneditable help formatpainter pageembed charmap mentions quickbars linkchecker emoticons advtable",
+				height: 240, // 编辑器高度(autoresize开启后无效)
+				max_height: 800, // 编辑器初始化最大高度
+				min_height: 360, // 编辑器初始化最小高度
+				readonly: true, //只读
+				branding: false, // 是否禁用“Powered by TinyMCE”
+				image_caption: false,
+				// images_upload_url: "",
+				// images_upload_base_path: "",
+				// images_upload_credentials: true,
+				images_reuse_filename: true,
+				images_upload_handler: function (blobInfo, success, failure) {
+					const token =
+						getToken() || localStorage.getItem("access_token"); //登录标示
+					const csrf_token = csrfToken();
+					var xhr, formData;
+					var file = blobInfo.blob(); //转化为易于理解的file对象
+					xhr = new XMLHttpRequest();
+					xhr.open(
+						"POST",
+						process.env.VUE_APP_SERVER_API + "/api/upload"
+					);
+					xhr.withCredentials = true;
+					xhr.setRequestHeader("authorization", "Bearer " + token);
+					xhr.setRequestHeader("x-csrf-token", csrf_token);
+					xhr.onload = function () {
+						var json;
+						if (xhr.status != 200) {
+							failure("HTTP Error: " + xhr.status);
+							return;
+						}
+						json = JSON.parse(xhr.responseText);
+						if (!json || json.code != 200) {
+							failure("Invalid JSON: " + xhr.responseText);
+							return;
+						}
+						success(process.env.VUE_APP_SERVER_API + json.data.url);
+					};
+					formData = new FormData();
+					formData.append(
+						"fields",
+						file,
+						file.name || "x." + file.type.match(/([^/]+)$/)[1]
+					); //此处与源文档不一样
+					xhr.send(formData);
 				},
-				menubar: "file edit view insert format tools table tc help",
+				file_picker_types: "media,image,file", // 想要哪一个图标提供本地文件选择功能，参数可为media(媒体)、image(图片)、file(文件)
+				//be used to add custom file picker to those dialogs that have it.
+				nonbreaking_force_tab: true, // 不间断空格
+				paste_as_text: false, //粘贴为文本
+				quickbars_selection_toolbar:
+					"bold italic | link h2 h3 blockquote", //选中快捷工具栏
+				quickbars_insert_toolbar: "quickimage quicktable", //快捷插入工具类
+				powerpaste_html_import: prompt, //clean - 保留内容的结构,merge保留原始文档的内联格式和结构,prompt清除和合并选项之间进行选择。
+				font_formats:
+					"微软雅黑=Microsoft YaHei; 宋体=SimSun; 新宋体=NSimSun; 仿宋=FangSong; 楷体=KaiTi; 黑体=SimHei; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Times New Roman=times new roman,times; Verdana=verdana,geneva;",
+				fontsize_formats:
+					"12px 14px 16px 18px 20px 22px 24px 28px 32px 36px 48px 60px 72px",
+				formats: {
+					alignleft: {
+						selector:
+							"p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img,video",
+						classes: "text-align-left",
+						styles: { "text-align": "left" },
+					},
+					aligncenter: {
+						selector:
+							"p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img,video",
+						classes: "text-align-center",
+						styles: { "text-align": "center" },
+					},
+					alignright: {
+						selector:
+							"p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table,img,video",
+						classes: "text-align-right",
+						styles: { "text-align": "right" },
+					},
+					alignjustify: {
+						selector:
+							"p,h1,h2,h3,h4,h5,h6,td,th,div,ul,ol,li,table",
+						classes: "text-align-justify",
+						styles: {
+							"text-align": "justify",
+							"text-align-last": "justify",
+							"text-justify": "distribute-all-lines",
+						},
+					},
+					bold: {
+						inline: "span",
+						classes: "font-weight-bold",
+						styles: { "font-weight": "bold" },
+					},
+					italic: {
+						inline: "span",
+						classes: "font-style-italic",
+						styles: { "font-style": "italic" },
+					},
+					underline: {
+						inline: "span",
+						classes: "text-decoration-underline",
+						styles: { "text-decoration": "underline" },
+						exact: true,
+					},
+					strikethrough: {
+						inline: "span",
+						classes: "text-decoration-line-through",
+						styles: { "text-decoration": "line-through" },
+						exact: true,
+					},
+					forecolor: {
+						inline: "span",
+						classes: "forecolor",
+						styles: { color: "%value" },
+					},
+					hilitecolor: {
+						inline: "span",
+						classes: "hilitecolor",
+						styles: { backgroundColor: "%value" },
+					},
+				},
+				menubar: "file edit insert view format table tools help", //顶部菜单栏显示
 				menu: {
 					file: {
 						title: "File",
@@ -133,7 +242,7 @@ export default {
 					format: {
 						title: "Format",
 						items:
-							"bold italic underline strikethrough superscript subscript codeformat | blockformats fontformats fontsizes align lineheight | forecolor backcolor | formatpainter removeformat",
+							"bold italic underline strikethrough superscript subscript codeformat | blockformats fontformats fontsizes align | forecolor backcolor | formatpainter removeformat",
 					},
 					tools: {
 						title: "Tools",
@@ -145,78 +254,15 @@ export default {
 						items:
 							"inserttable tableprops deletetable row column cell",
 					},
-					tc: {
-						title: "TinyComments",
-						items: "addcomment showcomments deleteallconversations",
-					},
 					help: {
 						title: "Help",
 						items: "help",
 					},
 				},
-				// toolbar:
-				// 	"undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment",
+				plugins:
+					"a11ychecker bdmap indent2em lineheight formatpainter powerpaste advlist anchor autolink autoresize autosave charmap code codesample directionality emoticons fullscreen help hr image imagetools importcss insertdatetime link lists media nonbreaking noneditable pagebreak preview print quickbars save searchreplace spellchecker tabfocus table template textpattern toc visualblocks visualchars wordcount",
 				toolbar:
-					"undo redo | formatselect | formatpainter a11ycheck | fontselect | fontsizeselect | bold italic underline strikethrough subscript superscript removeformat | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent indent2em lineheight | lists image media emoticons link charmap codesample table | bdmap | fullscreen",
-				autosave_ask_before_unload: true,
-				autosave_interval: "30s",
-				autosave_prefix: "{path}{query}-{id}-",
-				autosave_restore_when_empty: false,
-				autosave_retention: "2m",
-				image_advtab: true,
-				link_list: [
-					{ title: "My page 1", value: "http://www.tinymce.com" },
-					{ title: "My page 2", value: "http://www.moxiecode.com" },
-				],
-				image_list: [
-					{ title: "My page 1", value: "http://www.tinymce.com" },
-					{ title: "My page 2", value: "http://www.moxiecode.com" },
-				],
-				image_class_list: [
-					{ title: "None", value: "" },
-					{ title: "Some class", value: "class-name" },
-				],
-				importcss_append: true,
-				templates: [
-					{
-						title: "New Table",
-						description: "creates a new table",
-						content:
-							'<div class="mceTmpl"><table width="98%"  border="0" cellspacing="0" cellpadding="0"><tr><th scope="col"> </th><th scope="col"> </th></tr><tr><td> </td><td> </td></tr></table></div>',
-					},
-					{
-						title: "Starting my story",
-						description: "A cure for writers block",
-						content: "Once upon a time...",
-					},
-					{
-						title: "New list with dates",
-						description: "New List with dates",
-						content:
-							'<div class="mceTmpl"><span class="cdate">cdate</span><br /><span class="mdate">mdate</span><h2>My List</h2><ul><li></li><li></li></ul></div>',
-					},
-				],
-				template_cdate_format:
-					"[Date Created (CDATE): %m/%d/%Y : %H:%M:%S]",
-				template_mdate_format:
-					"[Date Modified (MDATE): %m/%d/%Y : %H:%M:%S]",
-				height: 600,
-				image_caption: true,
-				quickbars_selection_toolbar:
-					"bold italic | quicklink h2 h3 blockquote quickimage quicktable",
-				noneditable_noneditable_class: "mceNonEditable",
-				toolbar_mode: "sliding",
-				spellchecker_whitelist: ["Ephox", "Moxiecode"],
-				tinycomments_mode: "embedded",
-				content_style: ".mymention{ color: gray; }",
-				contextmenu:
-					"link image imagetools table configurepermanentpen",
-				a11y_advanced_options: true,
-				font_formats:
-					"微软雅黑=Microsoft YaHei; 宋体=SimSun; 新宋体=NSimSun; 仿宋=FangSong; 楷体=KaiTi; 黑体=SimHei; Arial=arial,helvetica,sans-serif; Arial Black=arial black,avant garde; Book Antiqua=book antiqua,palatino; Comic Sans MS=comic sans ms,sans-serif; Courier New=courier new,courier; Georgia=georgia,palatino; Helvetica=helvetica; Impact=impact,chicago; Symbol=symbol; Tahoma=tahoma,arial,helvetica,sans-serif; Times New Roman=times new roman,times; Verdana=verdana,geneva;",
-				fontsize_formats:
-					"12px 14px 16px 18px 20px 22px 24px 28px 32px 36px 48px 60px 72px",
-				file_picker_types: "media,image,file", // 想要哪一个图标提供本地文件选择功能，参数可为media(媒体)、image(图片)、file(文件)
+					"undo redo | formatselect | formatpainter a11ycheck | fontselect | fontsizeselect | bold italic underline strikethrough subscript superscript removeformat | forecolor backcolor | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent indent2em  lineheight | lists image media emoticons link charmap codesample table | bdmap | fullscreen",
 				setup: function (editor) {
 					console.log(`ID为:${editor.id}的编辑器即将初始化.`);
 				},
